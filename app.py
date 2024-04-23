654cd7d7f10ee41410a62c147f016c0f4e81c677
Jakub Szczypek <szczypek@student.agh.edu.pl>
2024-04-23 17:51:17 +0200

    Database changed to SQLAlchemy

--- app.py
+++ app.py
@@ -1,56 +1,49 @@
 from flask import Flask, redirect, render_template, url_for, request, flash, g, session
-import mysql.connector
-import os
+from datetime import date
+from datetime import datetime
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import Integer, String, Date, Text, Boolean
+
+import random
 import string
 import hashlib
 import binascii
-import random
 
 app = Flask(__name__)
 
 app.config['SECRET_KEY'] = 'SomethingWhatNoOneWillGuess'
 
+app.config.from_pyfile('config.cfg')
+
+Base = declarative_base()
+
+db = SQLAlchemy(model_class=Base)
+db.init_app(app)
+
 # Admin pass:
 # user name: onp
 # user pass: kMw
 
-# Dane do połączenia z bazą danych
-host = 'mysql.agh.edu.pl'
-port = 3306
-user = 'szczype2'
-password = 'Zpgbtj4kCY2kZikD'
-database = 'szczype2'
 
-# Funkcja do nawiązywania połączenia z bazą danych
-
-
-def get_db():
-    if 'db' not in g:
-        g.db = mysql.connector.connect(
-            host=host,
-            port=port,
-            user=user,
-            password=password,
-            database=database
-        )
-        print("Połączono z bazą danych MySQL")
-    return g.db
-
-# Funkcja do zamykania połączenia z bazą danych
-
-
-@app.teardown_appcontext
-def close_db(error):
-    db = g.pop('db', None)
-    if db is not None:
-        db.close()
-        print("Zamknięto połączenie z bazą danych MySQL")
+class users(db.Model):
+    __tablename__ = 'users'
+    id = db.Column(Integer, primary_key=True, autoincrement=True)
+    name = db.Column(String(100))
+    email = db.Column(String(100))
+    password = db.Column(Text)
+    is_active = db.Column(Boolean)
+    is_admin = db.Column(Boolean)
 
 
 class UserPass:
     def __init__(self, user='', password=''):
         self.user = user
         self.password = password
+        self.email = ''
+        self.is_admin = False
+        self.is_active = False
+        self.is_valid = False
 
     def hash_password(self):
         """Hash a password for storing."""
@@ -81,15 +74,11 @@ class UserPass:
         self.password = random_password
 
     def login_user(self):
-        db = get_db()
-        sql_statement = 'SELECT * FROM users WHERE name=%s'
-        cur = db.cursor(dictionary=True)
-        cur.execute(sql_statement, (self.user,))
-        user_record = cur.fetchone()
+        user_record = users.query.filter(users.name == self.user).first()
 
-        if user_record is not None and self.verify_password(user_record['password'], self.password):
-            session['user'] = user_record['name']
-            if user_record['is_admin']:
+        if user_record is not None and self.verify_password(user_record.password, self.password):
+            session['user'] = user_record.name
+            if user_record.is_admin:
                 session['is_admin'] = True
             return user_record
         else:
@@ -97,12 +86,33 @@ class UserPass:
             self.password = None
             return None
 
+    def get_user_info(self):
+        db_user = users.query.filter(users.name == self.user).first()
+
+        if db_user == None:
+            self.is_valid = False
+            self.is_admin = False
+            self.is_active = False
+            self.email = ''
+        elif db_user.is_active != 1:
+            self.is_valid = False
+            self.is_admin = False
+            self.is_active = False
+            self.email = db_user.email
+        else:
+            self.is_valid = True
+            self.is_admin = db_user.is_admin
+            self.is_active = db_user.is_active
+            self.email = db_user.email
+
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
+    login = UserPass(session.get('user'))  # type: ignore
+    login.get_user_info()
 
     if request.method == 'GET':
-        return render_template('login.html', active_menu='login')
+        return render_template('login.html', active_menu='login', login=login)
     else:
         user_name = '' if 'user_name' not in request.form else request.form['user_name']
         user_pass = '' if 'user_pass' not in request.form else request.form['user_pass']
@@ -116,7 +126,7 @@ def login():
             return redirect(url_for('index'))
         else:
             flash('Logon failed, try again')
-            return render_template('login.html')
+            return render_template('login.html', active_menu='login', login=login)
 
 
 @app.route('/logout')
@@ -129,24 +139,23 @@ def logout():
 
 @app.route('/init_app')
 def init_app():
-    db = get_db()
-    cursor = db.cursor()
-    sql_statement = 'SELECT COUNT(*) as cnt FROM users WHERE is_active and is_admin;'
-    cursor.execute(sql_statement)
-    active_admins = cursor.fetchone()
+    db.create_all()
 
-    if active_admins is not None and active_admins[0] > 0:
+    active_admins = users.query.filter(
+        users.is_active == True, users.is_admin == True).count()
+
+    if active_admins > 0:
         flash('Application is already set-up. Nothing to do')
         return redirect(url_for('index'))
 
     user_pass = UserPass()
     user_pass.get_random_user_password()
-    sql_statement = '''INSERT INTO users(name, email, password, is_active, is_admin)
-                    values(%s,%s,%s,True,True);'''
 
-    cursor.execute(sql_statement, [
-        user_pass.user, 'noone@nowhere.no', user_pass.hash_password()])
-    db.commit()
+    new_admin = users(name=user_pass.user, email='noone@nowhere.no',
+                      password=user_pass.hash_password(), is_active=True, is_admin=True)  # type: ignore
+    db.session.add(new_admin)
+    db.session.commit()
+
     flash('User {} with password {} has been created'.format(
         user_pass.user, user_pass.password))
     return redirect(url_for('index'))
@@ -154,31 +163,29 @@ def init_app():
 
 @app.route('/')
 def index():
-    return render_template('index.html', active_menu='index')
+    login = UserPass(session.get('user'))  # type: ignore
+    login.get_user_info()
+    return render_template('index.html', active_menu='home', login=login)
 
 
 @app.route('/register', methods=['GET', 'POST'])
 def register():
-    db = get_db()
+    login = UserPass(session.get('user'))  # type: ignore
+    login.get_user_info()
+
     message = None
     user = {}
     if request.method == 'GET':
-        return render_template('register.html', active_menu='register', user=user)
+        return render_template('register.html', active_menu='register', user=user, login=login)
     else:
         user['user_name'] = '' if 'user_name' not in request.form else request.form['user_name']
         user['email'] = '' if 'email' not in request.form else request.form['email']
         user['user_pass'] = '' if 'user_pass' not in request.form else request.form['user_pass']
 
-        cursor = db.cursor()
-        cursor.execute(
-            'SELECT COUNT(*) as cnt FROM users WHERE name = %s', [user['user_name']])
-        record = cursor.fetchone()
-        is_user_name_unique = (record['cnt'] == 0)
-
-        cursor.execute(
-            'SELECT COUNT(*) as cnt FROM users WHERE email = %s', [user['email']])
-        record = cursor.fetchone()
-        is_user_email_unique = (record['cnt'] == 0)
+        is_user_name_unique = (users.query.filter(
+            users.name == user['user_name']).count() == 0)
+        is_user_email_unique = (users.query.filter(
+            users.name == user['email']).count() == 0)
 
         if user['user_name'] == '':
             message = 'Name cannot be empty'
@@ -195,22 +202,24 @@ def register():
         if not message:
             user_pass = UserPass(user['user_name'], user['user_pass'])
             password_hash = user_pass.hash_password()
-            sql_statement = '''INSERT INTO users(name, email, password, is_active, is_admin)
-                                values(%s, %s, %s, True, False);'''
 
-            cursor.execute(sql_statement, [
-                user['user_name'], user['email'], password_hash])
-            db.commit()
+            new_user = users(name=user['user_name'], email=user['email'], password=password_hash,
+                             is_active=True, is_admin=False)  # type: ignore
+            db.session.add(new_user)
+            db.session.commit()
+
             flash('User {} created'.format(user['user_name']))
             return redirect(url_for('index'))
         else:
             flash('Error: {}'.format(message))
-            return render_template('register.html', active_menu='register', user=user)
+            return render_template('register.html', active_menu='register', user=user, login=login)
 
 
 @app.route('/repertoire')
 def repertoire():
-    return render_template('repertoire.html', active_menu='repertoire')
+    login = UserPass(session.get('user'))  # type: ignore
+    login.get_user_info()
+    return render_template('repertoire.html', active_menu='repertoire', login=login)
 
 
 if __name__ == '__main__':
