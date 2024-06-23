bedfdbc03badf94e540eeac965133ceb6de45191
Lucas5858 <l.szyszka5858@gmail.com>
2024-06-23 21:51:00 +0200

    Revert "zmiany zmiany"
    This reverts commit c22559900cd23dcb2d7d9ec13ac5402d975e0efe.

--- user_service/user.py
+++ user_service/user.py
@@ -326,7 +326,7 @@ def edit_your_account(user_name):
             db.session.commit()
             flash('Password was changed')
 
-        return redirect('http://127.0.0.1:8000/logout')
+        return redirect(url_for('your_account'))
 
 
 @user_app.route('/cancel_booking/<int:booking_id>', methods=['POST'])
